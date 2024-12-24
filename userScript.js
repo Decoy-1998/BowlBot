a6835f22c00f121610e22cd6c42aa778761bc26c
Decoy-1998 <the.real.hotwire.gaming@gmail.com>
2024-12-24 13:35:59 +0100

    Update userScript.js
    Added more message types to the listner

--- userScript.js
+++ userScript.js
@@ -14,9 +14,28 @@ function CustomChatRoomMessageListener(message) {
                 response = `You said a key word. Are you looking for help?`;
             } else if (message.Type === "Action") {
                 response = `I noticed your action involved A key word.`;
+            } else if (message.Type === "Activity") {
+                response = `I noticed your Activity involved A key word.`;
+            } else if (message.Type === "Whisper") {
+                response = `I noticed your Whisper involved A key word.`;
+            } else if (message.Type === "ServerMessage") {
+                response = `I noticed your ServerMessage involved A key word.`;
+            } else if (message.Type === "Emote") {
+                response = `I noticed your Emote involved A key word.`;
+            } else if (message.Type === "Hidden") {
+                response = `I noticed your Hidden involved A key word.`;
+            } else if (message.Type === "Status") {
+                response = `I noticed your Status involved A key word.`;
             } else {
                 response = `Interesting! A key word was mentioned in a "${message.Type}" message.`;
             }
+//Chat
+//Activity
+//Whisper
+//ServerMessage
+//Emote
+//Hidden
+//Status
 
             // Send the response message back to the chatroom
             ChatRoomSendLocal(response);
